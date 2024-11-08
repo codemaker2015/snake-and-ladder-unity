0248d2c2d1f227a0792cac518bc438b7e734f9e4
Vishnu <codemaker2015@gmail.com>
2024-11-08 21:43:12 +0530

    Initial commit

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    void Start(){
+
+    }
+    void Update(){
+
+    }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void oneP()
+    {
+        GameControl.nofPlayers = 1;
+        SceneManager.LoadScene("SnakeAndLadderMain");
+    }
+
+    public void twoP()
+    {
+        GameControl.nofPlayers = 2;
+        SceneManager.LoadScene("SnakeAndLadderMain");
+    }
+
+    public void threeP()
+    {
+        GameControl.nofPlayers = 3;
+        SceneManager.LoadScene("SnakeAndLadderMain");
+    }
+
+    public void fourP()
+    {
+        GameControl.nofPlayers = 4;
+        SceneManager.LoadScene("SnakeAndLadderMain");
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Snake And Ladder Unity
+
+The famous snake and ladder (chutes and ladder) game made using Unity Engine.
+
+![demo](Demos/demo.gif)
+
